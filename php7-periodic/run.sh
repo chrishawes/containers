c6c832cec4e29d56d97e4faa4af8419001dac15c
Chris Hawes <chris.hawes@basekit.com>
2017-07-12 13:44:16 +0100

    Add php7-periodic task runner

---
+++ php7-periodic/run.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+
+set -e
+
+if [ "${SCHEDULE}" = "None" ]; then
+  echo "You need to specify a schedule:
+  
+Entry                  | Description                                | Equivalent To
+-----                  | -----------                                | -------------
+@yearly (or @annually) | Run once a year, midnight, Jan. 1st        | 0 0 0 1 1 *
+@monthly               | Run once a month, midnight, first of month | 0 0 0 1 * *
+@weekly                | Run once a week, midnight on Sunday        | 0 0 0 * * 0
+@daily (or @midnight)  | Run once a day, midnight                   | 0 0 0 * * *
+@hourly                | Run once an hour, beginning of hour        | 0 0 * * * *
+ "
+else
+  exec go-cron -s "$SCHEDULE" -p $PORT -- $*
+fi
